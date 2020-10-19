75f673bd01e3a8fe092002427229c120a5614276
evikza <48568977+evikza@users.noreply.github.com>
2020-10-19 22:17:35 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once 'vendor/autoload.php';
+
+try {
+  $bot = new \TelegramBot\Api\Client(
+    '1154181768:AAEU8Rx6H7GeMYlrUkT3pysRfTCifEyPAg0'
+  );
+
+  $bot->command('/sendString', function ($message) use ($bot) {
+    $bot->sendMessage($message->getChat()->getId(), uniqid('', true));
+  });
+
+  $bot->run();
+} catch (\TelegramBot\Api\Exception $e) {
+  $e->getMessage();
+}
