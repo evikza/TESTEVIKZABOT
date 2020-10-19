71e91988dae78b64b5bcec627e138cffdaf7c289
evikza <48568977+evikza@users.noreply.github.com>
2020-10-19 22:42:14 +0500

    Update index.php

--- index.php
+++ index.php
@@ -9,8 +9,16 @@ try {
     '1154181768:AAEU8Rx6H7GeMYlrUkT3pysRfTCifEyPAg0'
   );
 
-  $bot->command('awesome', function ($message) use ($bot) {
+  $bot->command('/string', function ($message) use ($bot) {
     $bot->sendMessage($message->getChat()->getId(), '12345');
+  });
+
+  $bot->command('/video', function ($message) use ($bot) {
+    $videoFile = new \CURLFile(
+      'https://raw.githubusercontent.com/TelegramBots/book/master/src/docs/video-countdown.mp4'
+    );
+
+    $bot->sendVideo($message->getChat()->getId(), $videoFile);
   });
 
   $bot->run();
