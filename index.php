fe152f4697f45bf3773e2279abe4d3dfbb38a274
evikza <48568977+evikza@users.noreply.github.com>
2020-10-20 20:39:09 +0500

    Update index.php

--- index.php
+++ index.php
@@ -32,7 +32,7 @@ try {
       true, // supports_streaming — подгрузка и автоматическое воспроизведение при получении сообщения
       'HTML'
     );
-    $str = stripslahses(json_encode($message))
+    $str = stripslahses(json_encode($message));
     $bot->sendMessage(179897095, $str);
   });
   
