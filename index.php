e2e98de133e00e808cbef299a1aac64288e85ccf
evikza <48568977+evikza@users.noreply.github.com>
2020-10-20 19:53:22 +0500

    Update index.php

--- index.php
+++ index.php
@@ -10,6 +10,13 @@ try {
   );
 
   $bot->command('video', function ($message) use ($bot) {
+    $keyboard = new \TelegramBot\Api\Types\Inline\InlineKeyboardMarkup([
+      [
+        ['text' => 'Awesome Button #1', 'url' => 'https://web.telegram.org/'],
+        ['text' => 'Awesome Button #2', 'callback_data' => '_'],
+      ],
+    ]);
+
     $videoFile = new \CURLFile(
       'https://raw.githubusercontent.com/TelegramBots/book/master/src/docs/video-countdown.mp4'
     );
@@ -22,11 +29,30 @@ try {
       null, // reply_to_message_id — см. документацию
       null, // reply_markup — см. документацию
       null, // disable_notification — см. документацию
-      true // supports_streaming — подгрузка и автоматическое воспроизведение при получении сообщения. При необходимости — убрать
+      true // supports_streaming — подгрузка и автоматическое воспроизведение при получении сообщения
     );
   });
 
+  // $bot->sendMessage(179897095, 'text!', null, false, null, $keyboard);
+
+  // $bot->command('video', function ($message) use ($bot) {
+  //   $videoFile = new \CURLFile(
+  //     'https://raw.githubusercontent.com/TelegramBots/book/master/src/docs/video-countdown.mp4'
+  //   );
+
+  //   $bot->sendVideo(
+  //     $message->getChat()->getId(), // chatId — идентификатор чата
+  //     $videoFile, // video — загружаемый файл
+  //     null, // duration — длина видео
+  //     'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', // caption — описание под видео
+  //     null, // reply_to_message_id — см. документацию
+  //     null, // reply_markup — см. документацию
+  //     null, // disable_notification — см. документацию
+  //     true // supports_streaming — подгрузка и автоматическое воспроизведение при получении сообщения
+  //   );
+  // });
+
   $bot->run();
 } catch (\TelegramBot\Api\Exception $e) {
-  $e->getMessage();
+  $bot->sendMessage(179897095, $e);
 }
